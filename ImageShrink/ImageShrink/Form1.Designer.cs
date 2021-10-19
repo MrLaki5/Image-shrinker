f34ed6cd5dc4b56cf54c3a36cd68aa3020039e7b
dragan1810 <d.novakovic1810@yahoo.com>
2021-10-19 22:24:50 +0200

    added git ignore

--- ImageShrink/ImageShrink/Form1.Designer.cs
+++ ImageShrink/ImageShrink/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace ImageShrink
 {
@@ -137,7 +136,7 @@ namespace ImageShrink
 
         }
 
-        
+
 
         #endregion
 
